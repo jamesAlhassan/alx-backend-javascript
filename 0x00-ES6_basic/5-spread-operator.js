ef1f737134278db0ecdb4f116ee79e35e4787a39
James Alhassan <jamesalhassan052@gmail.com>
2023-06-26 19:52:18 +0000

    Update 5-spread-operator.js

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  const arr = [array1, array2, array2];
-  return [...arr];
+  const arr = [...array1, ...array2, ...string];
+  return arr;
 }
