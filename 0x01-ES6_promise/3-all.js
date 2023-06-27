bac4c6d24d382382bfe7671f6544368a15833538
James Alhassan <jamesalhassan052@gmail.com>
2023-06-27 20:32:37 +0000

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,10 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const user = createUser();
   const photo = uploadPhoto();
 
   return Promise.all([user, photo])
-    .then((res) =>
-      console.log(`${res[1].body} ${res[0].firstName} ${res[0].lastName}`)
-    )
-    .catch(() => console.log("Signup system offline"));
+    .then((res) => console.log(`${res[1].body} ${res[0].firstName} ${res[0].lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
