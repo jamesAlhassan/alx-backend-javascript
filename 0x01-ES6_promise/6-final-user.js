ae06d52da35a4ce2aea9aab83b748c05e6f3f09c
James Alhassan <jamesalhassan052@gmail.com>
2023-06-28 04:50:37 +0000

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
 
-  return Promise.allSettled([user, photo]).then((res) => console.log(res));
+  return Promise.allSettled([user, photo]).then((res) =>
+    res.map((item) => ({
+      status: item.status,
+      value: item.status === "fulfilled" ? item.value : String(item.reason),
+    }))
+  );
 }
