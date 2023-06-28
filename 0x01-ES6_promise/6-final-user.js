0dc51fb5f9e14ac6c1061743f7799e237ab24d60
James Alhassan <jamesalhassan052@gmail.com>
2023-06-28 04:56:19 +0000

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,9 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
 
-  return Promise.allSettled([user, photo]).then((res) =>
-    res.map((item) => ({
-      status: item.status,
-      value: item.status === "fulfilled" ? item.value : String(item.reason),
-    }))
-  );
+  return Promise.allSettled([user, photo]).then((res) => res.map((item) => ({ status: item.status, value: item.status === 'fulfilled' ? item.value : String(item.reason) })));
 }
