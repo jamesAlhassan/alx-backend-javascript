f0d25aa2b0eddddf330f801f6266731a17ec574c
James Alhassan <jamesalhassan052@gmail.com>
2023-07-04 18:30:29 +0000

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,4 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
-  const filteredArrayByCity = arr.filter((item) => item.city === city);
+  const filteredArrayByCity = arr.filter((item) => item.location === city);
   console.log(filteredArrayByCity);
 }
