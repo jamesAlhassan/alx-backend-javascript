e674b358fa28c654854d730e280d32cb2885dca8
James Alhassan <jamesalhassan052@gmail.com>
2023-07-04 21:48:37 +0000

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw Error("Position outside range");
+    throw Error('Position outside range');
   }
 
   const arrayBuffer = new ArrayBuffer(length);
