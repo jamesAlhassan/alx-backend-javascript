aae820617d2f246834282b3e4aa7b3acd5ea86bd
James Alhassan <jamesalhassan052@gmail.com>
2023-08-24 22:13:49 +0000

    Update 5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -15,3 +15,17 @@ describe('Hooks.', function () {
     // Runs after each test in this block.
     spyConsole.restore();
   });
+	it('Logs 120 with 100, 20.', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(spyConsole.calledOnce).to.be.true;
+  });
+
+  it('Logs 20 with 10, 10.', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(spyConsole.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(spyConsole.calledOnce).to.be.true;
+  });
+});
