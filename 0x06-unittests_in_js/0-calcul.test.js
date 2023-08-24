b044a99694126d84701e621f1d7f00ed0a565297
James Alhassan <jamesalhassan052@gmail.com>
2023-08-24 20:54:46 +0000

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -12,3 +12,13 @@ describe("Tests calculateNumber function:", function() {
             assert.strictEqual(calculateNumber(1, 3.7), 5);
         });
     });
+    describe("Calculates one float & one integer (round down):", function() {
+        it('Returns 4.', function() {
+            assert.strictEqual(calculateNumber(1, 3.3), 4);
+        });
+    });
+    describe("Calculates one float & one int reversed:", function () {
+    it('Returns 5.', function () {
+      assert.strictEqual(calculateNumber(3.7, 1), 5);
+    });
+  });
