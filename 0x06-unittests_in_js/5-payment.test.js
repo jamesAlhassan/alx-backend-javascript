b66782eefdd39c6b379b799441d579556a17d78f
James Alhassan <jamesalhassan052@gmail.com>
2023-08-24 22:13:12 +0000

    Update 5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -4,3 +4,14 @@ var { expect } = require('chai');
 const sendPaymentRequestToApi = require('./5-payment');
 //const Utils = require('./utils');
 
+describe('Hooks.', function () {
+  let spyConsole;
+  beforeEach(function () {
+    // Runs before each test in this block.
+    spyConsole = sinon.spy(console, 'log');
+  });
+
+  afterEach(function () {
+    // Runs after each test in this block.
+    spyConsole.restore();
+  });
