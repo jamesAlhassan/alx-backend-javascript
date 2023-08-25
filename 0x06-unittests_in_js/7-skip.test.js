0c476b85c50df0289137e59fa51a97cd131db230
James Alhassan <jamesalhassan052@gmail.com>
2023-08-25 04:58:32 +0000

    Update 7-skip.test.js

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -19,4 +19,15 @@ describe('Testing numbers', () => {
   it('4 is equal to 4', () => {
     expect(4 === 4).to.be.true;
   });
+it('5 is equal to 5', () => {
+    expect(5 === 5).to.be.true;
+  });
 
+  it('6 is equal to 6', () => {
+    expect(6 === 6).to.be.true;
+  });
+
+  it('7 is equal to 7', () => {
+    expect(7 === 7).to.be.true;
+  });
+});
