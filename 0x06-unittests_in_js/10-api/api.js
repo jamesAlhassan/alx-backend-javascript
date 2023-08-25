67b11654341974f0b101692e674d75d10bcd63aa
James Alhassan <jamesalhassan052@gmail.com>
2023-08-25 05:52:16 +0000

    Update api.js-- Add /cart

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -8,3 +8,9 @@ app.use(express.json());
 app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
