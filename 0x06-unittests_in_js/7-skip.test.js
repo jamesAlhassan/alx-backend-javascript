d61af5c6a86fb2cfcc8120d21bbe3113438ee82d
James Alhassan <jamesalhassan052@gmail.com>
2023-08-25 04:57:32 +0000

    Add 7-skip.test.js

---
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai');
+
+describe('Testing numbers', () => {
+  it('1 is equal to 1', () => {
+    expect(1 === 1).to.be.true;
+  });
+
+  it('2 is equal to 2', () => {
+    expect(2 === 2).to.be.true;
+  });
