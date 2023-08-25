ea8645e7d4f5bf3f1e5ca38e8397361cb5008df8
James Alhassan <jamesalhassan052@gmail.com>
2023-08-25 05:27:00 +0000

    Update api.test.js

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -17,3 +17,25 @@ describe('Integration Testing', () => {
     });
   });
 });
+
+describe('GET /cart/12', () => {
+    it('Responds with 200 and id 12', (done) => {
+      const options = {
+        url: 'http://localhost:7865/cart/12',
+        method: 'GET',
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Payment methods for cart 12');
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/1', () => {
+    it('Responds with 200 and id 1 in msg', (done) => {
+      const options = {
+        url: 'http://localhost:7865/cart/1',
+        method: 'GET',
+      };
